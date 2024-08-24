d92edea84a6a0cea110b652c86f92a9b3dd0f578
hentaimik <59136177+13582351091@users.noreply.github.com>
2024-08-23 22:31:58 -0700

    Update main.py

--- Subscriptable management novercel/main.py
+++ Subscriptable management novercel/main.py
@@ -1,4 +1,5 @@
 import uvicorn
 
+#这个部署linux
 if __name__ == "__main__":
-  uvicorn.run("api:app", host="0.0.0.0", port=5001, reload=True)
+  uvicorn.run("app:app", host="0.0.0.0", port=5001, reload=True)
