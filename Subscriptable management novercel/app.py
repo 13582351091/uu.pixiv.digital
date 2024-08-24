e370147cfd627baa21dcb5893e64349a20bfd357
hentaimik <59136177+13582351091@users.noreply.github.com>
2024-08-23 22:31:37 -0700

    Update and rename api.py to app.py

--- Subscriptable management novercel/app.py
+++ Subscriptable management novercel/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from randomSubscribeUrl import router as randomSubscribeUrlRouter
+import uvicorn
 
 app = FastAPI()
 #全局路由
 app.include_router(randomSubscribeUrlRouter, prefix="/randomSubscribeUrl")
 
-
+#这个是部署hf上
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=7860)
